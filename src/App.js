759d952c87d65a4886eaf73291ac004fa5496b38
rwparrish <rino3717@gmail.com>
2020-10-09 12:22:29 -0600

    create person component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>Hi, I'm a React App!!</h1>
-       <p>This is really working!</p>
+        <h1>Hi, I'm a React App!!</h1>
+        <p>This is really working!</p>
+        <Person />
       </div>
     );
     // the code above gets compiled to the code below. JSX above is much less cumbersome to write and easier to maintain. Importing 'React' allows us to write with JSX.
