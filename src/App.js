4f8eb2a6bb10083dd178ee15f7f46b6dd5e50a00
rwparrish <rino3717@gmail.com>
2020-10-09 15:53:11 -0600

    set up state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    people: [
+      { name: 'Ryan', age: 34 },
+      { name: 'Maria', age: 29 },
+      { name: 'Zoë', age: 8 },
+      { name: 'Adette', age: 6 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!!</h1>
         <p>This is really working!</p>
-        <Person name="Ryan" age="34" />
-        <Person name="Maria" age="29" >My Hobbies: I like to eat.</Person>
-        <Person name="Zoë" age="8"/>
-        <Person name="Adette" age="6"/>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
+        <button>Switch Name</button>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age} >My Hobbies: Food</Person>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
+        <Person name={this.state.people[3].name} age={this.state.people[3].age}/>
       </div>
     );
     // the code above gets compiled to the code below. JSX above is much less cumbersome to write and easier to maintain. Importing 'React' allows us to write with JSX.
