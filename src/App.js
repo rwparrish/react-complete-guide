7085583beee090cbc61bcab30714e023d5ec882b
rwparrish <rino3717@gmail.com>
2020-10-09 12:12:07 -0600

    create Parent folder for new component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App!!</h1>
+       <h1>Hi, I'm a React App!!</h1>
+       <p>This is really working!</p>
       </div>
     );
     // the code above gets compiled to the code below. JSX above is much less cumbersome to write and easier to maintain. Importing 'React' allows us to write with JSX.
