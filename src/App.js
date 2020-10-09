68863a3d453a39a8feda4d7844ffc7066d5b3c24
rwparrish <rino3717@gmail.com>
2020-10-09 16:39:46 -0600

    finish functional component change

--- src/App.js
+++ src/App.js
@@ -3,25 +3,45 @@ import './App.css';
 import Person from './Person/Person'
 
 const App = props => {
-  useState({
+  // React hooks 'useState' returns an array with two elements - ALWAYS - 1. current state 2. function to update state for React to rerender component
+  // when using Hooks the function responsible for changing state does not merge new state with old - it replaces it
+  const [ peopleState, setPeopleState ] = useState({
     people: [
       { name: 'Ryan', age: 34 },
       { name: 'Maria', age: 29 },
       { name: 'Zoë', age: 8 },
       { name: 'Adette', age: 6 }
     ],
-    otherState: 'some other value'
   });
+  
+  // when using React Hooks setState should be used multiple times for each 'slice' of state
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(peopleState, otherState);
+
+  const switchNameHandler = () => {
+    // console.log('Was Clicked');
+    // DON'T SET MUTATE DIRECTLY ->: this.state.people[0].name = 'Dr. Phill';
+    setPeopleState({
+      people: [
+        { name: 'Dr. Phill', age: 34 },
+        { name: 'Maria', age: 29 },
+        { name: 'Zoë', age: 8 },
+        { name: 'Adette', age: 7 }
+      ]
+    
+    })
+  }
 
   return (
     <div className="App">
       <h1>Hi, I'm a React App!!</h1>
       <p>This is really working!</p>
-      <Person name={this.state.people[0].name} age={this.state.people[0].age} />
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person name={this.state.people[1].name} age={this.state.people[1].age} >My Hobbies: Food</Person>
-      <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
-      <Person name={this.state.people[3].name} age={this.state.people[3].age}/>
+      <Person name={peopleState.people[0].name} age={peopleState.people[0].age} />
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={peopleState.people[1].name} age={peopleState.people[1].age} >My Hobbies: Food</Person>
+      <Person name={peopleState.people[2].name} age={peopleState.people[2].age}/>
+      <Person name={peopleState.people[3].name} age={peopleState.people[3].age}/>
     </div>
   );
     // the code above gets compiled to the code below. JSX above is much less cumbersome to write and easier to maintain. Importing 'React' allows us to write with JSX.
@@ -30,25 +50,5 @@ const App = props => {
 
 export default App;
 
-state = {
-  people: [
-    { name: 'Ryan', age: 34 },
-    { name: 'Maria', age: 29 },
-    { name: 'Zoë', age: 8 },
-    { name: 'Adette', age: 6 }
-  ]
-}
-
 // a 'method' is a function belonging to a class
-switchNameHandler = () => {
-  // console.log('Was Clicked');
-  // DON'T SET MUTATE DIRECTLY ->: this.state.people[0].name = 'Dr. Phill';
-  this.setState({
-    people: [
-      { name: 'Dr. Phill', age: 34 },
-      { name: 'Maria', age: 29 },
-      { name: 'Zoë', age: 8 },
-      { name: 'Adette', age: 7 }
-    ]
-  })
-}
+
