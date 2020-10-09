28ed1cae07b0a61dc5c4878a0f0093fde16f4ee7
rwparrish <rino3717@gmail.com>
2020-10-09 12:03:50 -0600

    compare JSX to react.createElement

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
         <h1>Hi, I'm a React App!!</h1>
       </div>
     );
+    // the code above gets compiled to the code below. JSX above is much less cumbersome to write and easier to maintain. Importing 'React' allows us to write with JSX.
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
