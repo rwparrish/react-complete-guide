71136191cc2aeae42ebf1cd8e8dfe3a3cdd31175
rwparrish <rino3717@gmail.com>
2020-10-13 10:18:08 -0600

    compare date passing using bind and inline arrow function

--- src/App.js
+++ src/App.js
@@ -1,54 +1,61 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const App = props => {
+class App extends Component {
+
+  state = { people: [
+    { name: 'Ryan', age: 34 },
+    { name: 'Maria', age: 29 },
+    { name: 'Zoë', age: 8 },
+    { name: 'Adette', age: 6 }
+  ] }
   // React hooks 'useState' returns an array with two elements - ALWAYS - 1. current state 2. function to update state for React to rerender component
   // when using Hooks the function responsible for changing state does not merge new state with old - it replaces it
-  const [ peopleState, setPeopleState ] = useState({
-    people: [
-      { name: 'Ryan', age: 34 },
-      { name: 'Maria', age: 29 },
-      { name: 'Zoë', age: 8 },
-      { name: 'Adette', age: 6 }
-    ],
-  });
-  
   // when using React Hooks setState should be used multiple times for each 'slice' of state
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(peopleState, otherState);
-
-  const switchNameHandler = () => {
+  switchNameHandler = ( newName ) => {
     // console.log('Was Clicked');
     // DON'T SET MUTATE DIRECTLY ->: this.state.people[0].name = 'Dr. Phill';
-    setPeopleState({
+    this.setState( {
       people: [
-        { name: 'Dr. Phill', age: 34 },
+        { name: newName, age: 34 },
         { name: 'Maria', age: 29 },
         { name: 'Zoë', age: 8 },
         { name: 'Adette', age: 7 }
       ]
-    
-    })
-  }
+    } )
+  };
+  
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App!!</h1>
-      <p>This is really working!</p>
-      <Person name={peopleState.people[0].name} age={peopleState.people[0].age} />
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={peopleState.people[1].name} age={peopleState.people[1].age} >My Hobbies: Food</Person>
-      <Person name={peopleState.people[2].name} age={peopleState.people[2].age}/>
-      <Person name={peopleState.people[3].name} age={peopleState.people[3].age}/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!!</h1>
+        <p>This is really working!</p>
+        <Person 
+          click={() => this.switchNameHandler('RyGuy')} 
+          name={this.state.people[0].name} 
+          age={this.state.people[0].age} />
+        <button onClick={this.switchNameHandler.bind(this, 'Dr. Phill')}>Switch Name</button>
+        <Person 
+          name={this.state.people[1].name} 
+          age={this.state.people[1].age} >My Hobbies: Food</Person>
+        <Person 
+          name={this.state.people[2].name} 
+          age={this.state.people[2].age}/>
+        <Person 
+          name={this.state.people[3].name} 
+          age={this.state.people[3].age}/>
+      </div>
+    );
+  }
     // the code above gets compiled to the code below. JSX above is much less cumbersome to write and easier to maintain. Importing 'React' allows us to write with JSX.
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
 }
 
 export default App;
+
+
 
 // a 'method' is a function belonging to a class
 
