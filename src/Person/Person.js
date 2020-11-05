f1b95ba1f23deea7dde5ff50e5c12f3e5da1f12d
rwparrish <rino3717@gmail.com>
2020-11-05 13:41:41 -0700

    build error boundary

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -20,6 +20,11 @@ import classes from "./Person.css"
 // `;
 
 const person = ( props ) => {
+    // below is an example error
+    // const rnd = Math.random();
+    // if (rnd > 0.7) {
+    //     throw new Error("Something went wrong")
+    // };
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
