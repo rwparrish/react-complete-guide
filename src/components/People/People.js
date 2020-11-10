515152741fb198d3ec56a7fb9567d7d765eafc5d
rwparrish <rino3717@gmail.com>
2020-11-10 10:12:44 -0700

    outsource People map to People component

---
+++ src/components/People/People.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Person from './Person/Person'
+
+const people = (props) => props.people.map((person, index) => {
+        return <Person
+          key={person.id}
+          click={() => props.clicked(index)}
+          name={person.name} 
+          age={person.age} 
+          nameChange={(event) => props.changed(event, person.id)} />
+      });
+
+    
+
+export default people;
